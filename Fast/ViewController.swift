66d1b685666022bfd8c6c81687e42f45dc366000
LongND9 <longnd9@fsoft.com.vn>
2017-02-07 17:30:58 +0700

    first commit

---
+++ Fast/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fast
+//
+//  Created by LongND9 on 2/7/17.
+//  Copyright © 2017 LongND9. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
